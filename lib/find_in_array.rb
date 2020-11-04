8f461bb5ede244ff3d95761044bb7874a6232ac0
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-04 21:56:06 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,8 @@
 def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    if array[count] == value_to_find
-      return count
+  length = array.length
+  length.times do |index|
+    if array[index] == value_to_find
+      return index
     end
-   count += 1
   end
 end
